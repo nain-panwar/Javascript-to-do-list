9983581f41f5aadafa7e3b7e8c14a05d1a1233e0
Nain Panwar <nainpanwar021@gmail.com>
2025-03-25 19:45:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 let tasks = ["Complete project", "Go to the gym", "Buy groceries", "Read a book"];
 let list = document.querySelector(".list");
+
 function displayTasks() {
+  list.innerHTML = ""; 
   tasks.forEach(task => {
     list.innerHTML += `<li>${task}</li>`;
   });
 }
+
 displayTasks();
+
 function addTask() {
-  let newTask = document.querySelector(".action input").value;
+  let newTaskInput = document.querySelector(".action input");
+  let newTask = newTaskInput.value.trim();
+
   if (newTask) {
     tasks.push(newTask);
-    displayTasks();
-    document.querySelector(".action input").value = "";
+    displayTasks(); 
+    newTaskInput.value = ""; 
   } else {
     alert("Please enter a task");
   }
